852cb518aac0ee369f34f954db33f6aec12ed3a3
Snow <51404519+sardor9@users.noreply.github.com>
2020-12-14 10:56:02 +0500

    Add files via upload

---
+++ Fit or Unfit Descision tree.py
@@ -0,0 +1,36 @@
+print("Age you higher 30 years?")
+print("Yes")
+print("Not")
+
+response = input("Please correct ansewer:")
+
+print()
+
+if response =="yes":
+    print("Eat lots of fast food?")
+    print("Yes")
+    print("Not")
+    
+    response = input("Please correct ansewer:")
+
+    print()
+    
+    if response =="yes":
+        print("Unfit")
+    elif response =="no":
+        print("Fit")
+    
+    
+elif response =="no":
+    print("Exercise in the morning daily")
+    print("Yes")
+    print("No")
+
+    response = input("Please correct ansewer:")
+
+    print()
+    if response =="yes":
+        print("Fit")
+    elif response == "no":
+        print("Unfit")
+       
